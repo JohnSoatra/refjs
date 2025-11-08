db39a7c23e21ccc6e41866122101965273d34632
soatra <soatra206.infinity@gmail.com>
2025-11-08 18:00:07 +0700

    add some new methods

--- src/constants/pickingMethods/array.ts
+++ src/constants/pickingMethods/array.ts
@@ -1,12 +1,18 @@
 export type PickingArrayMethods =
   | 'at'
   | 'find'
-  | 'findLast';
+  | 'findLast'
+  | 'pop'
+  | 'shift'
+  | 'unshift';
 
 const PickingArrayMethods = new Set<PickingArrayMethods>([
   'at',
   'find',
   'findLast',
+  'pop',
+  'shift',
+  'unshift',
 ]);
 
 export default PickingArrayMethods;
--- src/constants/producerMethods/array.ts
+++ src/constants/producerMethods/array.ts
@@ -1,5 +1,6 @@
 export type ProducerArrayMethods =
   | 'concat'
+  | 'filter'
   | 'flat'
   | 'slice'
   | 'splice'
@@ -10,6 +11,7 @@ export type ProducerArrayMethods =
 
 const ProducerArrayMethods = new Set<ProducerArrayMethods>([
   'concat',
+  'filter',
   'flat',
   'slice',
   'splice',
